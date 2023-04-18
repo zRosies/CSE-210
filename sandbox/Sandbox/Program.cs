194d46c77802e6deb6f8b0fc562d5a6280ee00f5
zRosies <gustavo.enrollment@gmail.com>
2023-04-18 11:22:36 -0300

    Changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,37 @@
 using System;
 
-class Program
+
+namespace game
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Sandbox World!!!");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Sandbox World!!!");
+            
+
+             //     Console.Write("What is your favorite color? ");
+                //     string color = Console.ReadLine()
+                // Console.Write("Choose a number:");
+                // string xstring = Console.ReadLine();
+                // int x = int.Parse(xstring);
+
+                // int y=3;
+                // if (x>y)
+                // {
+                //     Console.WriteLine($"The number you chose is {x}  is Correct");
+                // }
+                // else{
+                //     Console.WriteLine($"The number you chose is {x} is Incorrect");
+                // }
+                //    Console.Write("What is your name?");
+            string first = Console.ReadLine();
+            Console.Write("What is your last name?");
+            string last = Console.ReadLine();
+
+            Console.Write($"\nYour name is {last}, {first} {last}!");
+        }
+
+
     }
 }
