f46c52d11908bf61242c82acdd80beb1bd71f518
zRosies <gustavo.enrollment@gmail.com>
2023-04-24 20:38:57 -0300

    changes

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,35 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {    {
+        Random random = new Random();
+        int magicnum = random.Next(0,101);
+        int numgues=-1;
+        int guest= 0;
+        string play="";
+        while (numgues != magicnum || play=="yes"){
+            guest += 1;
+            Console.Write("What is the magic number?");
+            string guess = Console.ReadLine();
+            numgues= int.Parse(guess);
+            if (magicnum > numgues){
+                Console.WriteLine("Heigher");
+            }
+            else if (magicnum < numgues){
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine($"\nCongratulations, you've find the magic number. You took {guest} attempts!");
+                Console.Write("Do you want to play again?(yes/no)");
+                play=Console.ReadLine();
+            }
+            if (magicnum==numgues && play!="no"){
+                guest=0;
+                magicnum=random.Next(0,10);
+            }
+            
+        }
+      
+        }        
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int>numbers = numbers= new List<int>();
+        int number=-1;
+        while(number != 0){
+        Console.Write("Enter a number: ");
+        string user=Console.ReadLine();
+        int newuser=int.Parse(user);
+        numbers.Add(newuser);
+        }
     }
 }
