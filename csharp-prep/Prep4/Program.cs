cb30881e2ca8066835c36eb25ea7c24d391cfd6f
zRosies <gustavo.enrollment@gmail.com>
2023-04-24 21:18:59 -0300

    chantges

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,16 +1,25 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        List<int>numbers = numbers= new List<int>();
+        List<int> numbers = numbers= new List<int>();
         int number=-1;
         while(number != 0){
         Console.Write("Enter a number: ");
         string user=Console.ReadLine();
-        int newuser=int.Parse(user);
-        numbers.Add(newuser);
+        number=int.Parse(user);
+        numbers.Add(number);
+       
         }
+         foreach(int num in numbers){
+            int sum=0;
+            sum+=num;
+            Console.Write(sum);
+            }
+
+        
     }
 }
