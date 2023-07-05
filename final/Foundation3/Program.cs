88b55363c2186d868e2a2e49233237c5e7555150
zRosies <gustavo.enrollment@gmail.com>
2023-07-05 19:58:35 -0300

    changes

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        string[] menu ={"1. Create Event","2. Display Event"};
+        Lecture TEST= new Lecture("John",56,"The Power of the Water", "Lecture displaying the importance of the water","22.05.2023", "7 p.m","Googy 23 Street");
+        Reception test2= new Reception("")
+        Console.WriteLine("------------------------------------------------------------");
+        Console.WriteLine(TEST.LectureFullDetail());
+        Console.WriteLine("\n----------------------------------------------------------");
+        
     }
 }
